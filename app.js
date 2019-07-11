64c3b9391e4c604eabae9598d754151155c55130
Evgeniy Dainovich <zenbysel@gmail.com>
2019-07-11 18:03:56 +0300

    add tours route

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('shdflsd');
   res.status(200).send('hello');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    data: { tours }
+  });
 });
 
 app.post('/', (req, res) => {
