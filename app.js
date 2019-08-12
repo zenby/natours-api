bb081f79a0050919d4080263e6fae1fe3d04d62f
Evgeniy Dainovich <zenbysel@gmail.com>
2019-08-13 00:35:44 +0300

    add env variables

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+dotenv.config({ path: './config.env' });
 
 app.get('/', (req, res) => {
   res.status(200).send('hello');
 });
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  /* eslint-disable no-console */
   console.log(`App running on port ${port}`);
 });
