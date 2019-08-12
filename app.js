210f455b032349dc9401855c46e7b9d8061abc3c
Evgeniy Dainovich <zenbysel@gmail.com>
2019-08-13 00:23:59 +0300

    add static files serving

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
