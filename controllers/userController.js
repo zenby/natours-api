c28e2b0d2e7068af40ec21356157a825b7cd62fe
Evgeniy Dainovich <zenbysel@gmail.com>
2019-08-12 23:49:22 +0300

    refactor file structure

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).send({
+    status: 'error',
+    message: 'this route is not implemented'
+  });
+};
+exports.createUser = (req, res) => {
+  res.status(500).send({
+    status: 'error',
+    message: 'this route is not implemented'
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).send({
+    status: 'error',
+    message: 'this route is not implemented'
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).send({
+    status: 'error',
+    message: 'this route is not implemented'
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).send({
+    status: 'error',
+    message: 'this route is not implemented'
+  });
+};
