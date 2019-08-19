36143ce0b9ad9a8610809a0db0d78a1f232b8472
Evgeniy Dainovich <zenbysel@gmail.com>
2019-08-20 00:32:30 +0300

    add getTours methods

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,10 +1,18 @@
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
-  // 1 variant
-  // const newTour = new Tour({});
-  // newTour.save();
-  // 2 variant
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      data: { tour: tours }
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'failed',
+      message: e
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
@@ -15,12 +23,28 @@ exports.deleteTour = (req, res) => {
   res.status(200).end();
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).end();
+exports.getTour = async (req, res) => {
+  try {
+    // Tour.findOne({ _id: req.params.id });
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: { tour: tour }
+    });
+  } catch (e) {
+    res.status(404).json({
+      status: 'failed',
+      message: e
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
   try {
+    // 1 variant
+    // const newTour = new Tour({});
+    // newTour.save();
+    // 2 variant
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
