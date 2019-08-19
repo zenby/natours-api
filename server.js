5228318346ec6e16096e3de77884eeab2871e0aa
Evgeniy Dainovich <zenbysel@gmail.com>
2019-08-19 22:53:11 +0300

    add mongoose connection

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+console.log(process.env.NODE_ENV);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
