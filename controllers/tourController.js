25085dcf097b8df145769fe1e8900761772057d0
Evgeniy Dainovich <zenbysel@gmail.com>
2019-08-20 21:52:27 +0300

    add populate script

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -80,7 +80,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'failed',
-      message: 'Invalid data sent'
+      message: error
     });
   }
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,8 +6,41 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true
   },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] }
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingAverage: { type: Number, default: 4.5 },
+  ratingQuantity: { type: Number, default: 0 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
